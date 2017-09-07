55c5d13b97f3dfc4eb4a82c4785d1b68112a53d1
ironicsushi <yuli5565@colorado.edu>
2017-09-07 12:21:46 -0500

    *fixed append issue to append to different divs *changed color of h3 font

--- weather_map.js
+++ weather_map.js
@@ -10,11 +10,12 @@ $.get("http://api.openweathermap.org/data/2.5/forecast/?lat=29.42.42&lon=-98.49"
     cnt: 3
 }).done(function(data){
     console.log(data);
+    var weatherArray = ["#present", "#one-day", "#two-day"];
+    var weather = "";
+    var j = 0;
+
     // WEATHER BOXES
-
     (data.list).forEach(function(daily){
-            var weather = "";
-
             var icon = daily.weather[0].icon;
             console.log(icon);
             var url = "http://openweathermap.org/img/w/" + icon + ".png";
@@ -30,9 +31,12 @@ $.get("http://api.openweathermap.org/data/2.5/forecast/?lat=29.42.42&lon=-98.49"
             "<li><strong>Humidity</strong>: " + daily.main.humidity + "%</li>" +
             "<li><strong>Wind</strong>: " + Math.round(daily.wind.speed) + " mph</li>" +
             "<li><strong>Pressure</strong>: " + daily.main.pressure + " hPa</li>" +
-            "</ul>" + weather;
+            "</ul>";
 
-            $("#present").append(weather);
+            $(weatherArray[j]).append(weather);
+            console.log(weatherArray[j]);
+            j++;
+
         });
 
 });
