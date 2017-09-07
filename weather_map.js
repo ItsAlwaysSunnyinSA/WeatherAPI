9235c6881cc12d9b7846c9040735a70465052742
alxmpotter <alxmpotter@gmail.com>
2017-09-07 11:36:57 -0500

    weather api
    *start of forEach function
    *switch get to 5 day forecast

--- weather_map.js
+++ weather_map.js
@@ -3,29 +3,39 @@
 // (function(){
 // })();
 
-$.get("http://api.openweathermap.org/data/2.5/forecast?lat=29.42.42&lon=-98.49", {
+$.get("http://api.openweathermap.org/data/2.5/forecast/?lat=29.42.42&lon=-98.49", {
     APPID: "14585416169d73d956f6321f3f0c5253",
     q: "San Antonio, TX",
     units: "imperial",
     cnt: 3
 }).done(function(data){
     console.log(data);
-    var icon = data.weather[0].icon;
-    console.log(icon);
-    var url = "http://openweathermap.org/img/w/" + icon + ".png";
-    var img = "<img class='icon' src='" + url + "'>";
-    console.log(img);
     // WEATHER BOXES
     // temperature (max/min)
-    var presentWeather =
-        "<h3>" + Math.round(data.main.temp_max) + "°/" + Math.round(data.main.temp_min) + "°</h3>" +
-        img +
-    //list for weather info
-        "<ul>" +
-        "<li><strong>Humidity</strong>: " + data.main.humidity + "%</li>" +
-        "<li><strong>Wind</strong>: " + Math.round(data.wind.speed) + " mph</li>" +
-        "<li><strong>Pressure</strong>: " + data.main.pressure + " hPa</li>" +
-        "</ul>";
+
+        data.forEach(function(daily, index){
+            var presentWeather = "";
+
+            var icon = data.list[index].weather[0].icon;
+            console.log(icon);
+            var url = "http://openweathermap.org/img/w/" + icon + ".png";
+            var img = "<img class='icon' src='" + url + "'>";
+            console.log(img);
+
+            presentWeather =+
+
+            "<h3>" + Math.round(data.list[index].main.temp_max) + "°/" + Math.round(data.list[index].main.temp_min) + "°</h3>" +
+             img +
+            //list for weather info
+            "<ul>" +
+            "<li><strong>Humidity</strong>: " + data.list[index].main.humidity + "%</li>" +
+            "<li><strong>Wind</strong>: " + Math.round(data.list[index].wind.speed) + " mph</li>" +
+            "<li><strong>Pressure</strong>: " + data.list[index].main.pressure + " hPa</li>" +
+            "</ul>";
+
+            $("#present").append(presentWeather);
+        });
+
     // icon
 
     // clouds
@@ -35,6 +45,5 @@ $.get("http://api.openweathermap.org/data/2.5/forecast?lat=29.42.42&lon=-98.49",
 
     //pressure
 
-    $("#present").append(presentWeather);
 });
 
