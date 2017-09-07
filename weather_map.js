ba3a5521d8b5d9eaae130de60260f18cc0afbb92
ironicsushi <yuli5565@colorado.edu>
2017-09-07 15:55:12 -0500

    *moved function to callWeather *added click event for button

--- weather_map.js
+++ weather_map.js
@@ -3,19 +3,24 @@
 // (function(){
 // })();
 
-$.get("http://api.openweathermap.org/data/2.5/forecast/?lat=29.42&lon=-98.49", {
-    APPID: "14585416169d73d956f6321f3f0c5253",
-    q: "San Antonio, TX",
-    units: "imperial",
-    cnt: 3
-}).done(function(data){
-    console.log(data);
-    var weatherArray = ["#present", "#one-day", "#two-day"];
-    var weather = "";
-    var j = 0;
 
-    // WEATHER BOXES
-    (data.list).forEach(function(daily){
+var callWeather = function () {
+    $.get("http://api.openweathermap.org/data/2.5/forecast", {
+        APPID: "14585416169d73d956f6321f3f0c5253",
+        lat: $("#lat-input").val(),
+        lon: $("#lon-input").val(),
+        units: "imperial",
+        cnt: 3
+    }).done(function (data) {
+        console.log(data);
+        console.log($("#lat-input").val());
+        console.log($("#lon-input").val());
+        var weatherArray = ["#present", "#one-day", "#two-day"];
+        var weather = "";
+        var j = 0;
+
+        // WEATHER BOXES
+        (data.list).forEach(function (daily) {
             var icon = daily.weather[0].icon;
             console.log(icon);
             var url = "http://openweathermap.org/img/w/" + icon + ".png";
@@ -24,32 +29,38 @@ $.get("http://api.openweathermap.org/data/2.5/forecast/?lat=29.42&lon=-98.49", {
 
             weather =
 
-            "<h3>" + Math.round(daily.main.temp_max) + "°/" + Math.round(daily.main.temp_min) + "°</h3>" +
-             img +
-            //list for weather info
-            "<ul>" +
-            "<li><strong>Humidity</strong>: " + daily.main.humidity + "%</li>" +
-            "<li><strong>Wind</strong>: " + Math.round(daily.wind.speed) + " mph</li>" +
-            "<li><strong>Pressure</strong>: " + daily.main.pressure + " hPa</li>" +
-            "</ul>";
+                "<h3>" + Math.round(daily.main.temp_max) + "°/" + Math.round(daily.main.temp_min) + "°</h3>" +
+                img +
+                //list for weather info
+                "<ul>" +
+                "<li><strong>Humidity</strong>: " + daily.main.humidity + "%</li>" +
+                "<li><strong>Wind</strong>: " + Math.round(daily.wind.speed) + " mph</li>" +
+                "<li><strong>Pressure</strong>: " + daily.main.pressure + " hPa</li>" +
+                "</ul>";
 
             $(weatherArray[j]).append(weather);
             console.log(weatherArray[j]);
             j++;
 
+            });
+
         });
 
-});
+    };
 
-var mapOptions = {
+    $("#weather-submit").click(callWeather);
 
-    zoom: 5,
+    var mapOptions = {
 
-    center: {
-        lat: 29.42,
-        lng: -98.49
-    }
-};
+        zoom: 5,
 
-var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+        center: {
+            lat: 29.42,
+            lng: -98.49
+        }
+    };
+
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+
 
