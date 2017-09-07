1c042af8f20e6daacbc20df7ec83fa4b9d3fb970
alxmpotter <alxmpotter@gmail.com>
2017-09-07 09:37:58 -0500

    weather api
    -created get request

---
+++ weather_map.js
@@ -0,0 +1,12 @@
+"use strict";
+//IFIE function
+// (function(){
+// })();
+
+$.get("http://api.openweathermap.org/data/2.5/weather", {
+    APPID: "14585416169d73d956f6321f3f0c5253",
+    q: "San Antonio, TX",
+    units: "imperial"
+}).done(function(data){
+    console.log(data);
+});
